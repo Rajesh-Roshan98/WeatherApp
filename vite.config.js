88bdeb6fb16689e32bd3e3dbed38697ebffcd245
Rajesh-Roshan98 <rrajeshroshan89@gmail.com>
2025-10-07 21:24:43 +0530

    Weather app is Completed

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    // Increase chunk size warning limit (in KB)
+    chunkSizeWarningLimit: 1000, // e.g., 1000 KB = 1 MB
+  },
 })
